e2bd572f3993c75147533dd9f3e8139271a5d842
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2024-07-28 14:32:50 +0200

    make trixie and sway work

--- dev.sh
+++ dev.sh
@@ -2,4 +2,4 @@
 
 echo building dev image
 podman build --target dev --tag tellerrand-dev .
-exec podman run -it --rm -p 5901:5901 -v ./:/app tellerrand-dev bash
+exec podman run -it --rm -p 5901:5901 -v ./:/app -v ./sway/:/etc/sway/ tellerrand-dev bash
